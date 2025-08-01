e70a84177d2b4913a7a7d3411433ace44a0e81ea
Letícia Farage <162373412+lefarage@users.noreply.github.com>
2025-08-01 12:40:39 +0000

    numeros na senha

--- main.js
+++ main.js
@@ -26,4 +26,10 @@ const campoSenha = document.querySelector('#campo-senha');
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÇ';
 
+function geraSenha(){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor (numeroAleatorio);
+    console.log(numeroAleatorio)
+}
+
 campoSenha.value = letrasMaiusculas;
